8b01fb9bddcde5dd8a317e47f88601f544eb5afa
Tina Albores <maalbores@gmail.com>
2018-08-02 15:50:16 +0800

    ruby: simplified the two-fer solution further after receiving feedback

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,9 +1,7 @@
 class TwoFer
 
-  def self.two_fer(name = nil)
-    recipient = name || "you"
-
-    "One for #{recipient}, one for me."
+  def self.two_fer(name = "you")
+    "One for #{name}, one for me."
   end
 
 end
