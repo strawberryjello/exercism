bd75f02dce030690f1a558b65ac37b4f319f68aa
Tina Albores <maalbores@gmail.com>
2018-08-08 17:06:53 +0800

    ruby: bob: refactored the elsifs into a set of guard clauses, extracted methods, removed the unused hash

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,31 +1,26 @@
 class Bob
 
-  RESPONSES = {
-    :question => "Sure.",
-    :yell => "Whoa, chill out!",
-    :yelled_question => "Calm down, I know what I'm doing!",
-    :silence => "Fine. Be that way!",
-    :default => "Whatever."
-  }
-
   def self.hey(string)
     remark = string.strip
 
-    if remark == ""
-      "Fine. Be that way!"
-    elsif is_yelling?(remark) && remark.end_with?("?")
-      "Calm down, I know what I'm doing!"
-    elsif is_yelling?(remark)
-      "Whoa, chill out!"
-    elsif remark.end_with?("?")
-      "Sure."
-    else
-      "Whatever."
-    end
+    return "Fine. Be that way!" if remark == ""
+    return "Calm down, I know what I'm doing!" if is_yelling_a_question?(remark)
+    return "Whoa, chill out!" if is_yelling?(remark)
+    return "Sure." if is_asking_a_question?(remark)
+
+    "Whatever."
   end
 
   def self.is_yelling?(string)
     !(string =~ /[a-z]/) && (string =~ /[A-Z]/)
+  end
+
+  def self.is_yelling_a_question?(string)
+    is_yelling?(string) && is_asking_a_question?(string)
+  end
+
+  def self.is_asking_a_question?(string)
+    string.end_with?("?")
   end
 
 end
